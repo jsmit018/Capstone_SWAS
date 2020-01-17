47d58b7f7b1cd7d42df5273728c1db51e3d2cf64
auman001 <57813549+auman001@users.noreply.github.com>
2020-01-17 10:19:31 -0500

    Add files via upload

---
+++ resources.cpp
@@ -0,0 +1,22 @@
+#include "resources.h"
+
+void Resource::virtualAcquire()
+{
+}
+
+void Resource::virtualRelease()
+{
+}
+
+bool Resource::isAvailable()
+{
+	return false;
+}
+
+void Resource::failResource()
+{
+}
+
+void Resource::restoreResource()
+{
+}
