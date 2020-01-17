021289a92dfeebdaef34e316147c0d93b38730f5
auman001 <57813549+auman001@users.noreply.github.com>
2020-01-17 10:47:00 -0500

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,35 @@
+#include "task.h"
+
+void Task::SetNextTask(Task * nextTask)
+{
+}
+
+void Task::Arrive(Entity * entity)
+{
+}
+
+void Task::SetName(string name)
+{
+}
+
+string Task::GetName()
+{
+	return string();
+}
+
+int Task::GetID()
+{
+	return 0;
+}
+
+Task::Task(string name)
+{
+}
+
+void Task::Depart(Entity * entity)
+{
+}
+
+void Task::TaskProcess(Entity * entity)
+{
+}
