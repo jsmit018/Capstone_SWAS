90de08ac50f66990a30cdbcb161a495862bee341
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-01-21 09:04:10 -0500

    Update step.h
    Updated keywords and naming conventions. Added several functions and corresponding classes.

--- step.h
+++ step.h
@@ -5,21 +5,29 @@
 class Step : public Task
 {
 public:
-	Step();
-	VirtualExecutive();
-	Time GetServiceTime();
-	void SetServiceTime(Time serviceTime);
+	Step(Distribution serviceTime, string name);
+	void Execute(Airplane* airplane);
+	//Time GetServiceTime();
+	//void SetServiceTime(Time serviceTime);
 	string GetName();
 	void SetName(string name);
 	int GetNumberInQueue();
-	void SetNumberInQueue(int NumberInQueue);
-	void AcquireServerEM();
+	//void SetNumberInQueue(int NumberInQueue);
+	/*void AcquireServerEM();
 	void AddQueueEM();
-	void ScheduleDoneServiceEM();
+	void ScheduleDoneServiceEM();*/
 	void ReleaseServer();
 	void DoneService();
 private:
-	Time serviceTime;
+	//Time _serviceTime;
+	Distribution* _serviceTime;
 	string _name;
-	int NumberInQueue;
+	int _numberInQueue;
+	
+	class AcquireServerEA;
+	class AddQueueEA;
+	class ScheduleDoneServiceEA;
+	void AcquireServerEM(Airplane* airplane);
+	void AddQueueEM(Airplane* airplane);
+	void ScheduleDoneServiceEM(Airplane* airplane);
 };
