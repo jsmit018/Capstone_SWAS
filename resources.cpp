6e11b82e068a032fcefc0b8b7191eeb34418b3d9
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-01-21 08:52:50 -0500

    Update resources.cpp

--- resources.cpp
+++ resources.cpp
@@ -1,22 +1,42 @@
 #include "resources.h"
 
-void Resource::virtualAcquire()
+//@TODO will need to figure out logic for what happens if amount needed is greater
+void Resource::Acquire(int amountNeeded)
 {
+	if (amountNeeded > _resourceCount)
+		//do some stuff
+	else{
+		_resourceCount -= amountNeeded;
+	}
+	
+	if (_resourceCount < _threshold){
+		//Schedule an ordering event... may need to have an EA/EM with this class for that
+	}
 }
 
-void Resource::virtualRelease()
+void Resource::Release(int amountToRelease)
 {
+	_resourceCount += amountToRelease;
 }
 
-bool Resource::isAvailable()
+
+bool Resource::isAvailable(int amountNeeded)
 {
-	return false;
+	if (amountNeeded > _resourceCount)
+		return false;
+	else{
+		return true;
+	}
 }
 
 void Resource::failResource()
 {
+	//@TODO write the algorithm for a resource failure essentially is just scheduling an event
+	//so needed EA and EM
 }
 
 void Resource::restoreResource()
 {
+	//May probably need an associated Event to Execute function call. 
+	_resourceCount++;
 }
