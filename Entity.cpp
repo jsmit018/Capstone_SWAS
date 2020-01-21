0edbe9999bfad73a9985a6b74bca0aebe3c5c900
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-01-21 07:13:17 -0500

    Update Entity.cpp
    Filled in basic getters and setters added constructor stuff.
    
    Next Commit: Adding New() function to update ID.

--- Entity.cpp
+++ Entity.cpp
@@ -1,49 +1,55 @@
 #include "Entity.h"
 
-Airplane::Airplane() {
+int Airplane::_nextID = 0;
 
+Airplane::Airplane(string aircraftType, int priority) {
+    _aircraftType = aircraftType;
+    _priority = priority;
 }
 
 int Airplane::GetEntityID() {
-
+    return _entityID;
 }
 
 void Airplane::SetEntityID(int  entityID) {
-
+    _entityID = entityID;
 }
 
 int Airplane::GetSource() {
-
+     return _source;
 }
 
 void Airplane::SetSource(int source) {
-
+    _source = source;
 }
 
 Footprint Airplane::GetFootprint() {
-
+     return _dimensions;
 }
 
 void Airplane::SetFootprint(Footprint dimensions) {
-
+    _dimensions = dimensions;
 }
 
 string Airplane::GetAircraftType() {
-
+    return _aircraftType;
 }
 
-void Airplane::SetAircraftType(string aircraftType) {
+//Going to leave this commented for now as this should be taken care of in the constructor
+/*void Airplane::SetAircraftType(string aircraftType) {
 
-}
+}*/
 
 int Airplane::GetPriority() {
-
+  return _priority;
 }
 
-void Airplane::SetPriority(int priority) {
+//Going to leave this commented for now as this should be taken care of in the constructor
+/*void Airplane::SetPriority(int priority) {
 
-}
+}*/
 
+//Going to leave this for now but essentially this will update very time a new airplane is made
 int Airplane::GetNextID() {
-
+    return _nextID;
 }
