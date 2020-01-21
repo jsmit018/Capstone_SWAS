f70d4f6ce38cd35c500ce5a7b5604996b1cdeb21
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-01-21 09:09:31 -0500

    Update Step.cpp
    Added newly updated and changed functions and classes.

--- Step.cpp
+++ Step.cpp
@@ -1,52 +1,80 @@
 #include "step.h"
 
-Step::Step()
+Step::Step(Distribution* serviceTime, string name)
 {
+	_serviceTime = serviceTime;
+	_name = name;
 }
 
-Time Step::GetServiceTime()
+class Step::AcquireServerEA : public EventAction {
+public:
+
+private:
+};
+
+class Step::ScheduleDoneServiceEA : public EventAction{
+public:
+
+private:
+};
+
+class Step::AddQueueEA : public EventAction{
+public:
+	
+private:
+};
+
+//Since Service Time is now a distribution no need to get and retrieve.
+/*Time Step::GetServiceTime()
 {
 	return Time();
 }
 
 void Step::SetServiceTime(Time serviceTime)
 {
-}
+}*/
 
 string Step::GetName()
 {
-	return string();
+	return _name;
 }
 
 void Step::SetName(string name)
 {
+	_name = name;
 }
 
 int Step::GetNumberInQueue()
 {
-	return 0;
+	return _numberInQueue;
 }
 
-void Step::SetNumberInQueue(int NumberInQueue)
+//We won't ever need this function as this will update throughout the simulation.
+/*void Step::SetNumberInQueue(int NumberInQueue)
+{
+}*/
+
+void Step::AcquireServerEM(Airplane* airplane)
 {
 }
 
-void Step::AcquireServerEM()
+void Step::AddQueueEM(Airplane* airplane)
 {
 }
 
-void Step::AddQueueEM()
+void Step::ScheduleDoneServiceEM(Airplane* airplane)
 {
 }
 
-void Step::ScheduleDoneServiceEM()
-{
-}
-
+//@TODO figure out what these are supposed to be... EM?
 void Step::ReleaseServer()
 {
 }
 
 void Step::DoneService()
 {
+}
+
+void Step::Execute(Airplane* airplane){
+	
 }
