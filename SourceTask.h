4deadf76877072b60f94431e98df250510ed3a11
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-01-22 07:10:54 -0500

    Update SourceTask.h
    Added a variable, changed a keyword and updated the constructor.

--- SourceTask.h
+++ SourceTask.h
@@ -4,26 +4,28 @@
 
 class SourceBlock : public Task {
 public:
-	Source();
-	Time GetInterarrivalTime();
-	void SetInterarrivalTime();
+	Source((Distribution* interarrivalTime, string aircraftType, int numberOfAircraftToGenerate
+	Time timeForFirstAircraft, Aircraft* aircraft);
+	//Time GetInterarrivalTime();
+	//void SetInterarrivalTime(Time interarrivalTime);
 	string GetAircraftType();
 	void SetAircraftType(string aircraftType);
 	int GetNumberOfAircraftToGenerate();
-	void SetNumberOfAircraftToGenerate(int numberToGenerate);
+	//void SetNumberOfAircraftToGenerate(int numberToGenerate);
 	string GetName();
 	void SetName(string name);
 	Time GetTimeForFirstAircraft();
 	void SetTimeforFirstAircraft(Time setTime);
 	int GetNumberGenerated();
-	void SetNumberGenerated(int numberGenerated);
+	//void SetNumberGenerated(int numberGenerated);
 	void Exeucte(Airplane* airplane);
 private:
-	Time _interarrivalTime;
+	Distribution* _interarrivalTime;
+	Aircraft* _aircraft;
 	string _aircraftType;
 	int _numberOfAircraftToGenerate;
 	string _name;
-	Time _timeForFirstAircraft;
+	//Time _timeForFirstAircraft;
 	int _numberGenerated;
 	class ScheduleNextEntityEA;
 	void ScheduleNextEntityEM(Airplane* airplane);
