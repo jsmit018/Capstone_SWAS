436136edeed439b6d1e40eb19c8ae1dae1198991
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-01-22 06:38:15 -0500

    Update SinkTask.cpp
    Updated Sink Logic - Status: Testing

--- SinkTask.cpp
+++ SinkTask.cpp
@@ -1,17 +1,21 @@
 #include "SinkTask.h"
 
-SinkBlock::SinkBlock() : Task() {
-
+SinkBlock::SinkBlock() : Task(name) {
+    _name = name;
 }
 
 string SinkBlock::GetName() {
-
+    return _name;
 }
 
 void SinkBlock::SetName(string name) {
-
+    _name = name;
 }
 
 int SinkBlock::GetTerminatedAircraft() {
+    _return _terminatedAircraft;
+}
 
+void SinkBlock::Execute(Aircraft* aircraft){
+   _terminatedAircraft++; 
 }
