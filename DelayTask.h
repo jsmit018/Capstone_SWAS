023792ed16d26b234ce200aafd3d94fd1b576d1b
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-01-27 11:04:37 -0500

    Update DelayTask.h
    Updated the .h class file to appropriate keywords, naming conventions, and parameters

--- DelayTask.h
+++ DelayTask.h
@@ -4,19 +4,19 @@
 
 class DelayBlock : public Task {
 public:
-	DelayBlock();
-	Time GetDelayTime();
-	void SetDelayTime(Time delayTime);
+	DelayBlock(Distribution* delayTime, string name);
+	//Time GetDelayTime();
+	//void SetDelayTime(Time delayTime);
 	string GetName();
 	void SetName(string name);
-	void Execute(Airplane* airplane);
+	void Execute(Aircraft* aircraft);
 	
 
 private:
-	Time delayTime;
+	Distribution* _delayTime;
 	string _name;
 	class StartDelayEA;
 	class EndDelayEA;
-	void StartDelayEM(Airplane* airplane);
-	void EndDelayEM(Airplane* airplane);
+	void StartDelayEM(Aircraft* aircraft);
+	void EndDelayEM(Aircraft* aircraft);
 };
