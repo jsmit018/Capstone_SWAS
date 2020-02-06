3cbafb894324ac8dcfc25cbae90dca45e8957ac4
Tyler Lee <clee008@odu.edu>
2020-02-06 18:07:18 -0500

    Updates to Unity GUI Project

---
+++ SWAS/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public GameObject cameraTarget;
+    private float targetX;
+    private float targetY;
+
+    // Initialize Camera Size Based on Warehouse Dimensions
+    void Start()
+    {
+        targetX = cameraTarget.transform.localScale.x;
+        targetY = cameraTarget.transform.localScale.y;
+
+        Camera camera = GetComponent<Camera>();
+
+        if (targetX > targetY)
+        {
+            camera.orthographicSize = targetX * 2.7f;
+        }
+        else
+        {
+            camera.orthographicSize = targetY * 2.7f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
