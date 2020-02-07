dbf87efd1d3a54cea6c55e162fa9ef3d4727aae4
Tyler Lee <clee008@odu.edu>
2020-02-06 21:13:03 -0500

    Drag and Drop Behavior Working on Raycasters

---
+++ SWAS/Assets/Scripts/DropHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NewBehaviourScript : MonoBehaviour, IDropHandler
+{
+    public GameObject scenePanel;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //RectTransform toolboxPanel = transform as RectTransform;
+        RectTransform sceneCull = scenePanel.GetComponent<RectTransform>();
+
+        if(RectTransformUtility.RectangleContainsScreenPoint(sceneCull, Input.mousePosition))
+        {
+            Debug.Log("Dropped");
+        }
+    }
+}
---
+++ SWAS/Assets/Scripts/RayCast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    public Camera sceneCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // do we hit our scene plane?
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject);
+
+                var localPoint = hit.textureCoord;
+                // convert the hit texture coordinates into camera coordinates
+                //Ray portalRay = sceneCamera.ScreenPointToRay(new Vector2(localPoint.x * sceneCamera.pixelWidth, localPoint.y * sceneCamera.pixelHeight));
+                Ray sceneRay = sceneCamera.ViewportPointToRay(hit.textureCoord);
+                RaycastHit sceneHit;
+                // test these camera coordinates in another raycast test
+                if (Physics.Raycast(sceneRay, out sceneHit))
+                {
+                    Debug.Log(sceneHit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
