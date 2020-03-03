d4892e80672897adc0b05c70200ccdea7da00e4a
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-03-03 07:04:10 -0500

    Update CalendarConverter.cpp

--- SWAS/CalendarConverter.cpp
+++ SWAS/CalendarConverter.cpp
@@ -18,4 +18,15 @@ void CalConverter::InsertDays(int month, int numDays)
 //		cout << it->first << " " << it->second << "\n";
 	}
 }
-
+/*
+//Decimal is put first, integer or whole number is put second
+or 
+/>>> a = 1234.5678
+>>> (lambda x, y: (int(x), int(x*y) % y/y))(a, 1e0)
+(1234, 0.0)
+>>> (lambda x, y: (int(x), int(x*y) % y/y))(a, 1e1)
+(1234, 0.5)
+>>> (lambda x, y: (int(x), int(x*y) % y/y))(a, 1e15)
+(1234, 0.5678) /
+ x, y = math.modf();
+*/
