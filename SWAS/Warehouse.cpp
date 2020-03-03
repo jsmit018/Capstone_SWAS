79dd60d30b69ffd9c5c9ec0ae8c160681d800d6d
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2020-03-03 08:26:17 -0500

    Update Warehouse.cpp

--- SWAS/Warehouse.cpp
+++ SWAS/Warehouse.cpp
@@ -7,20 +7,24 @@ Warehouse::Warehouse(double length, double width) {
 
 double Warehouse::GetLength()
 {
+    cout << "Returning Hangar Length" << endl;
     return _hangarLength;
 }
 
 void Warehouse::SetLength(double hangarLength)
 {
+    cout << "Updating Hangar length" << endl;
     _hangarLength = hangarLength;
 }
 
 void Warehouse::SetWidth(double warehouseWidth)
 {
+    cout << "Updating Hangar Width" << endl;
     _hangarWidth = warehouseWidth;
 }
 
 double Warehouse::GetWidth()
 {
+    cout << "Returning Hangar Width" << endl;
     return _hangarWidth;
 }
