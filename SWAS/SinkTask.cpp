abf9b5f03d6829405c94932ab9c6c767234b1486
rmm013 <57813506+rmm013@users.noreply.github.com>
2020-04-03 11:12:15 -0700

    Add files via upload
    SinkTask class with code documentation.

--- SWAS/SinkTask.cpp
+++ SWAS/SinkTask.cpp
@@ -2,26 +2,40 @@
 #include "SinkTask.h"
 #include "Aircraft.h"
 
+/**
+* Constructor for sink
+*/
 SinkBlock::SinkBlock(string name) : Task(name) {
-    _name = name;
+	_name = name;
 }
 
+/**
+* Returns the sink name
+*/
 string SinkBlock::GetName() {
-    return _name;
+	return _name;
 }
 
+/**
+* Sets the sink name
+*/
 void SinkBlock::SetName(string name) {
-    _name = name;
+	_name = name;
 }
 
+/**
+* Returns the terminated aircraft
+*/
 int SinkBlock::GetTerminatedAircraft() {
-    cout << "Returning terminated aircraft" << endl;
-    return _terminatedAircraft;
+	cout << "Returning terminated aircraft" << endl;
+	return _terminatedAircraft;
 }
 
+/**
+* Prints the aircraft type and ID, increments the number of terminated aircraft, and deletes the aircraft
+*/
 void SinkBlock::Execute(Aircraft* aircraft) {
-   cout << "Aircraft, " << aircraft->GetAircraftType() << " number " << aircraft->GetAircraftID() << "has finished tour of maintenance, departing" << endl;
-   _terminatedAircraft++; 
-   delete aircraft;
+	cout << "Aircraft, " << aircraft->GetAircraftType() << " number " << aircraft->GetAircraftID() << "has finished tour of maintenance, departing" << endl;
+	_terminatedAircraft++;
+	delete aircraft;
 }
-
--- SWAS/SinkTask.h
+++ SWAS/SinkTask.h
@@ -3,12 +3,30 @@
 //#include "SimObj.h"
 #include "SimExec.h"
 
+/**
+* Class for SinkTask that inherits from Task
+*/
 class SinkBlock : public Task {
 public:
+	/**
+	* Constructor for sink
+	*/
 	SinkBlock(string name);
+	/**
+	* Deletes aircraft
+	*/
 	void Execute(Aircraft* aircraft);
+	/**
+	* Returns the sink name
+	*/
 	string GetName();
+	/**
+	* Sets the sink name
+	*/
 	void SetName(string name);
+	/**
+	* Returns the terminated aircraft
+	*/
 	int GetTerminatedAircraft();
 private:
 	string _name;
