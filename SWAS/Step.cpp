f7adaa621eced2f31a25657c2055935f5b892344
Kevin Voyt <kvoyt001@odu.edu>
2020-04-21 04:37:28 -0400

    Revert "commit"
    This reverts commit 6a049d8a996044307ad3ba8c012991fa20a80f0d.

--- SWAS/Step.cpp
+++ SWAS/Step.cpp
@@ -1351,14 +1351,10 @@ void Step::DoneServiceEM(Aircraft* aircraft, vector<string> acquiredResources)
 			else
 			{
 				//cout << " here 4" << endl;
-				//Empty resources from the last job
-				map<string, Resource*>::const_iterator iter = aircraft->GetMyRepairJobObj(_myRJ)->GetStep(_stepID - 1)->GetResourceMapBegin();
-				while (iter != aircraft->GetMyRepairJobObj(_myRJ)->GetStep(_stepID - 1)->GetResourceMapEnd())
+				map<string, Resource*>::const_iterator iter = _reqResourceMap.begin();
+				while (iter != _reqResourceMap.end())
 				{
-					if (iter == aircraft->GetMyRepairJobObj(_myRJ)->GetStep(_stepID - 1)->GetResourceMapEnd())
-					{
-						break;
-					}
+
 					cout << ".................ID: " << aircraft->GetAircraftID() << "Releasing " << iter->first << endl;
 					//SimExec::ScheduleEventAt(_RJpriority, new ReleaseResourceEA(this, iter->second), 0.0, "ReleaseResourceEA");
 					ReleaseResourceEM(iter->second, iter->second->GetNumResNeeded());
