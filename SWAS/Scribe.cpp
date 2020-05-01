09e14807ae561cb0547202ff10476391b44b9161
Andrea Robey <Andrea.robey@simisinc.com>
2020-05-01 12:29:33 -0400

    commented out the sql code in archive so that all using master wont need a database connection

--- SWAS/Scribe.cpp
+++ SWAS/Scribe.cpp
@@ -1353,112 +1353,112 @@ void Scribe::Archive()
 	////// CONNECTING TO DATABASE //////
 	////////////////////////////////////
 
-	#define SQL_RESULT_LEN 240
-	#define SQL_RETURN_CODE_LEN 1000
+	//#define SQL_RESULT_LEN 240
+	//#define SQL_RETURN_CODE_LEN 1000
 
-	//define handles and variables
-	SQLHANDLE sqlConnHandle;
-	SQLHANDLE sqlStmtHandle;
-	SQLHANDLE sqlEnvHandle;
-	SQLWCHAR retconstring[SQL_RETURN_CODE_LEN];
+	////define handles and variables
+	//SQLHANDLE sqlConnHandle;
+	//SQLHANDLE sqlStmtHandle;
+	//SQLHANDLE sqlEnvHandle;
+	//SQLWCHAR retconstring[SQL_RETURN_CODE_LEN];
 
-	//initializations
-	sqlConnHandle = NULL;
-	sqlStmtHandle = NULL;
+	////initializations
+	//sqlConnHandle = NULL;
+	//sqlStmtHandle = NULL;
 
-	//allocations
-	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlEnvHandle))
-		goto COMPLETED;
+	////allocations
+	//if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlEnvHandle))
+	//	goto COMPLETED;
 
-	if (SQL_SUCCESS != SQLSetEnvAttr(sqlEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
-		goto COMPLETED;
+	//if (SQL_SUCCESS != SQLSetEnvAttr(sqlEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
+	//	goto COMPLETED;
 
-	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, sqlEnvHandle, &sqlConnHandle))
-		goto COMPLETED;
-
-
-	//output
-	cout << "Attempting connection to SQL Server...";
-	cout << "\n";
-
-	//connect to SQL Server	
-	//Using a trusted connection and port 14808
-	//it does not matter if you are using default or named instance
-	//just make sure you define the server name and the port
-	//We have the option to use a username/password instead of a trusted connection
-	//but its more secure to use a trusted connection
-	switch (SQLDriverConnect(sqlConnHandle,
-		NULL,
-		//(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=master;UID=username;PWD=password;",
-
-		//********* Need to set server and database names -->automate this in GUI (if there is one)
-		(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=govasim-2;DATABASE=SWASTestDatabase;Trusted=true;",
-		SQL_NTS,
-		retconstring,
-		1024,
-		NULL,
-		SQL_DRIVER_NOPROMPT)) {
-
-	case SQL_SUCCESS:
-		cout << "Successfully connected to SQL Server";
-		cout << "\n";
-		break;
-
-	case SQL_SUCCESS_WITH_INFO:
-		cout << "Successfully connected to SQL Server";
-		cout << "\n";
-		break;
-
-	case SQL_INVALID_HANDLE:
-		cout << "Could not connect to SQL Server";
-		cout << "\n";
-		goto COMPLETED;
-
-	case SQL_ERROR:
-		cout << "Could not connect to SQL Server";
-		cout << "\n";
-		goto COMPLETED;
-
-	default:
-		break;
-	}
-
-	//if there is a problem connecting then exit application
-	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, sqlConnHandle, &sqlStmtHandle))
-		goto COMPLETED;
-
-	//output
-	cout << "\n";
-	cout << "Executing T-SQL query...";
-	cout << "\n";
-
-	//if there is a problem executing the query then exit application
-	//else display query result
-
-	////////////////////////////////////////////////////
-	//THIS IS WHERE THE CODE FOR INSERTING WILL GO:
-	if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)L"SELECT name FROM artists", SQL_NTS)) {   ///<<<<<<<<< THIS is an example of SQL Query code that finds the values from aircraft name and prints.
-		cout << "Error querying SQL Server";
-		cout << "\n";
-		goto COMPLETED;
-	}
-	else {
-
-		//declare output variable and pointer
-		SQLCHAR sqlValue[SQL_RESULT_LEN];
-		SQLINTEGER ptrSqlValue;
-
-		while (SQLFetch(sqlStmtHandle) == SQL_SUCCESS) {
-
-			SQLGetData(sqlStmtHandle, 1, SQL_CHAR, sqlValue, SQL_RESULT_LEN, &ptrSqlValue);
-
-			//display query result
-			cout << "\nQuery Result:\n\n";
-			cout << sqlValue << endl;
+	//if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, sqlEnvHandle, &sqlConnHandle))
+	//	goto COMPLETED;
 
 
-		}
-	}
+	////output
+	//cout << "Attempting connection to SQL Server...";
+	//cout << "\n";
+
+	////connect to SQL Server	
+	////Using a trusted connection and port 14808
+	////it does not matter if you are using default or named instance
+	////just make sure you define the server name and the port
+	////We have the option to use a username/password instead of a trusted connection
+	////but its more secure to use a trusted connection
+	//switch (SQLDriverConnect(sqlConnHandle,
+	//	NULL,
+	//	//(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=master;UID=username;PWD=password;",
+
+	//	//********* Need to set server and database names -->automate this in GUI (if there is one)
+	//	(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=govasim-2;DATABASE=SWASTestDatabase;Trusted=true;",
+	//	SQL_NTS,
+	//	retconstring,
+	//	1024,
+	//	NULL,
+	//	SQL_DRIVER_NOPROMPT)) {
+
+	//case SQL_SUCCESS:
+	//	cout << "Successfully connected to SQL Server";
+	//	cout << "\n";
+	//	break;
+
+	//case SQL_SUCCESS_WITH_INFO:
+	//	cout << "Successfully connected to SQL Server";
+	//	cout << "\n";
+	//	break;
+
+	//case SQL_INVALID_HANDLE:
+	//	cout << "Could not connect to SQL Server";
+	//	cout << "\n";
+	//	goto COMPLETED;
+
+	//case SQL_ERROR:
+	//	cout << "Could not connect to SQL Server";
+	//	cout << "\n";
+	//	goto COMPLETED;
+
+	//default:
+	//	break;
+	//}
+
+	////if there is a problem connecting then exit application
+	//if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, sqlConnHandle, &sqlStmtHandle))
+	//	goto COMPLETED;
+
+	////output
+	//cout << "\n";
+	//cout << "Executing T-SQL query...";
+	//cout << "\n";
+
+	////if there is a problem executing the query then exit application
+	////else display query result
+
+	//////////////////////////////////////////////////////
+	////THIS IS WHERE THE CODE FOR INSERTING WILL GO:
+	//if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)L"SELECT name FROM artists", SQL_NTS)) {   ///<<<<<<<<< THIS is an example of SQL Query code that finds the values from aircraft name and prints.
+	//	cout << "Error querying SQL Server";
+	//	cout << "\n";
+	//	goto COMPLETED;
+	//}
+	//else {
+
+	//	//declare output variable and pointer
+	//	SQLCHAR sqlValue[SQL_RESULT_LEN];
+	//	SQLINTEGER ptrSqlValue;
+
+	//	while (SQLFetch(sqlStmtHandle) == SQL_SUCCESS) {
+
+	//		SQLGetData(sqlStmtHandle, 1, SQL_CHAR, sqlValue, SQL_RESULT_LEN, &ptrSqlValue);
+
+	//		//display query result
+	//		cout << "\nQuery Result:\n\n";
+	//		cout << sqlValue << endl;
+
+
+	//	}
+	//}
 	//////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////
 
@@ -2024,15 +2024,15 @@ void Scribe::Archive()
 	/////////////////////////////////////////
 
 	//close connection and free resources
-	COMPLETED:
-		SQLFreeHandle(SQL_HANDLE_STMT, sqlStmtHandle);
-		SQLDisconnect(sqlConnHandle);
-		SQLFreeHandle(SQL_HANDLE_DBC, sqlConnHandle);
-		SQLFreeHandle(SQL_HANDLE_ENV, sqlEnvHandle);
+	//COMPLETED:
+	//	SQLFreeHandle(SQL_HANDLE_STMT, sqlStmtHandle);
+	//	SQLDisconnect(sqlConnHandle);
+	//	SQLFreeHandle(SQL_HANDLE_DBC, sqlConnHandle);
+	//	SQLFreeHandle(SQL_HANDLE_ENV, sqlEnvHandle);
 
-	//pause the console window - exit when key is pressed
-	cout << "\nPress enter key to exit...";
-	getchar();
+	////pause the console window - exit when key is pressed
+	//cout << "\nPress enter key to exit...";
+	//getchar();
 
 
 	//////////////////////////////////////////////////////////
