4679cfbf9731d4c63745d795cc03c76ef59b799e
Kevin Voyt <kvoyt001@odu.edu>
2020-05-02 17:01:34 -0400

    Added example line to output data to SQL database (I think)

--- SWAS/Scribe.cpp
+++ SWAS/Scribe.cpp
@@ -7,6 +7,8 @@
 #include <sqltypes.h>
 #include <sql.h>
 
+
+
 //Scribe static initializers
 runNode* Scribe::runStart = nullptr;
 
@@ -1344,123 +1346,124 @@ void Scribe::Archive()
 {
 	//out file stream variable
 	ofstream fileOut;
+	//sql::Connection *conn;
 	//temporary string for formatting
 	string tempStr = "";
 	//integer to count end of list pointers
 	int endCount;
 
-	////////////////////////////////////
+	/*//////////////////////////////////
 	////// CONNECTING TO DATABASE //////
 	////////////////////////////////////
 
-	//#define SQL_RESULT_LEN 240
-	//#define SQL_RETURN_CODE_LEN 1000
+	#define SQL_RESULT_LEN 240
+	#define SQL_RETURN_CODE_LEN 1000
 
-	////define handles and variables
-	//SQLHANDLE sqlConnHandle;
-	//SQLHANDLE sqlStmtHandle;
-	//SQLHANDLE sqlEnvHandle;
-	//SQLWCHAR retconstring[SQL_RETURN_CODE_LEN];
+	//define handles and variables
+	SQLHANDLE sqlConnHandle;
+	SQLHANDLE sqlStmtHandle;
+	SQLHANDLE sqlEnvHandle;
+	SQLWCHAR retconstring[SQL_RETURN_CODE_LEN];
 
-	////initializations
-	//sqlConnHandle = NULL;
-	//sqlStmtHandle = NULL;
+	//initializations
+	sqlConnHandle = NULL;
+	sqlStmtHandle = NULL;
 
-	////allocations
-	//if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlEnvHandle))
-	//	goto COMPLETED;
+	//allocations
+	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlEnvHandle))
+		goto COMPLETED;
 
-	//if (SQL_SUCCESS != SQLSetEnvAttr(sqlEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
-	//	goto COMPLETED;
+	if (SQL_SUCCESS != SQLSetEnvAttr(sqlEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
+		goto COMPLETED;
 
-	//if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, sqlEnvHandle, &sqlConnHandle))
-	//	goto COMPLETED;
+	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, sqlEnvHandle, &sqlConnHandle))
+		goto COMPLETED;
 
 
-	////output
-	//cout << "Attempting connection to SQL Server...";
-	//cout << "\n";
+	//output
+	cout << "Attempting connection to SQL Server...";
+	cout << "\n";
 
-	////connect to SQL Server	
+	//connect to SQL Server	
 	////Using a trusted connection and port 14808
 	////it does not matter if you are using default or named instance
 	////just make sure you define the server name and the port
 	////We have the option to use a username/password instead of a trusted connection
-	////but its more secure to use a trusted connection
-	//switch (SQLDriverConnect(sqlConnHandle,
-	//	NULL,
-	//	//(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=master;UID=username;PWD=password;",
+	//but its more secure to use a trusted connection
+	switch (SQLDriverConnect(sqlConnHandle,
+		NULL,
+		//(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=master;UID=username;PWD=password;",
 
-	//	//********* Need to set server and database names -->automate this in GUI (if there is one)
-	//	(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=govasim-2;DATABASE=SWASTestDatabase;Trusted=true;",
-	//	SQL_NTS,
-	//	retconstring,
-	//	1024,
-	//	NULL,
-	//	SQL_DRIVER_NOPROMPT)) {
+		//********* Need to set server and database names -->automate this in GUI (if there is one)
+		(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=govasim-2;DATABASE=SWASTestDatabase;Trusted=true;",
+		SQL_NTS,
+		retconstring,
+		1024,
+		NULL,
+		SQL_DRIVER_NOPROMPT)) {
 
-	//case SQL_SUCCESS:
-	//	cout << "Successfully connected to SQL Server";
-	//	cout << "\n";
-	//	break;
+	case SQL_SUCCESS:
+		cout << "Successfully connected to SQL Server";
+		cout << "\n";
+		break;
 
-	//case SQL_SUCCESS_WITH_INFO:
-	//	cout << "Successfully connected to SQL Server";
-	//	cout << "\n";
-	//	break;
+	case SQL_SUCCESS_WITH_INFO:
+		cout << "Successfully connected to SQL Server";
+		cout << "\n";
+		break;
 
-	//case SQL_INVALID_HANDLE:
-	//	cout << "Could not connect to SQL Server";
-	//	cout << "\n";
-	//	goto COMPLETED;
+	case SQL_INVALID_HANDLE:
+		cout << "Could not connect to SQL Server";
+		cout << "\n";
+		goto COMPLETED;
 
-	//case SQL_ERROR:
-	//	cout << "Could not connect to SQL Server";
-	//	cout << "\n";
-	//	goto COMPLETED;
+	case SQL_ERROR:
+		cout << "Could not connect to SQL Server";
+		cout << "\n";
+		goto COMPLETED;
 
-	//default:
-	//	break;
-	//}
+	default:
+		break;
+	}
 
-	////if there is a problem connecting then exit application
-	//if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, sqlConnHandle, &sqlStmtHandle))
-	//	goto COMPLETED;
+	//if there is a problem connecting then exit application
+	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, sqlConnHandle, &sqlStmtHandle))
+		goto COMPLETED;
 
-	////output
-	//cout << "\n";
-	//cout << "Executing T-SQL query...";
-	//cout << "\n";
+	//output
+	cout << "\n";
+	cout << "Executing T-SQL query...";
+	cout << "\n";
 
-	////if there is a problem executing the query then exit application
-	////else display query result
+	//if there is a problem executing the query then exit application
+	//else display query result
 
 	//////////////////////////////////////////////////////
-	////THIS IS WHERE THE CODE FOR INSERTING WILL GO:
-	//if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)L"SELECT name FROM artists", SQL_NTS)) {   ///<<<<<<<<< THIS is an example of SQL Query code that finds the values from aircraft name and prints.
-	//	cout << "Error querying SQL Server";
-	//	cout << "\n";
-	//	goto COMPLETED;
-	//}
-	//else {
+	//THIS IS WHERE THE CODE FOR INSERTING WILL GO:
+	if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)L"SELECT name FROM artists", SQL_NTS)) {   ///<<<<<<<<< THIS is an example of SQL Query code that finds the values from aircraft name and prints.
+		cout << "Error querying SQL Server";
+		cout << "\n";
+		goto COMPLETED;
+	}
+	else {
 
-	//	//declare output variable and pointer
-	//	SQLCHAR sqlValue[SQL_RESULT_LEN];
-	//	SQLINTEGER ptrSqlValue;
+		//declare output variable and pointer
+		SQLCHAR sqlValue[SQL_RESULT_LEN];
+		SQLINTEGER ptrSqlValue;
 
-	//	while (SQLFetch(sqlStmtHandle) == SQL_SUCCESS) {
+		while (SQLFetch(sqlStmtHandle) == SQL_SUCCESS) {
 
-	//		SQLGetData(sqlStmtHandle, 1, SQL_CHAR, sqlValue, SQL_RESULT_LEN, &ptrSqlValue);
+			SQLGetData(sqlStmtHandle, 1, SQL_CHAR, sqlValue, SQL_RESULT_LEN, &ptrSqlValue);
 
-	//		//display query result
-	//		cout << "\nQuery Result:\n\n";
-	//		cout << sqlValue << endl;
+			//display query result
+			cout << "\nQuery Result:\n\n";
+			cout << sqlValue << endl;
 
 
-	//	}
-	//}
+		}
+	}
 	//////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////*/
 
 	//Begin Archive
 	fileOut.open(fileName);
@@ -1526,6 +1529,10 @@ void Scribe::Archive()
 			{
 				//record both type and count
 				tempStr += ((runCurrent->aircraftRunner->type) + "," + to_string(runCurrent->aircraftRunner->count) + ",");
+				
+				
+				//Example Line
+				//SQLExecDirect( sqlStmtHandle,(SQLWCHAR*)("INSERT INTO Aircraft (Type, Number) VALUES ( .('" + (runCurrent->aircraftRunner->type) + "')., '" + to_string(runCurrent->aircraftRunner->count) + "')").c_str(), SQL_NTS);
 				runCurrent->aircraftRunner = runCurrent->aircraftRunner->next;
 			}
 			//advance run
@@ -1535,6 +1542,7 @@ void Scribe::Archive()
 		tempStr += "\n";
 		//Record Line
 		fileOut << tempStr;
+		
 		//check that all lists have ended
 	} while (endCount < runNumber);
 	//Above should result in a blank line separating above from missions below
